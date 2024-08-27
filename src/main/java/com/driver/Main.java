e6e2feb90c708e973797237525a8709a611c07fd
Mukul Vasanth <mukulvasanth@gmail.com>
2024-08-27 09:24:22 +0530

    Updated code changes for polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,32 @@ package com.driver;
 
 public class Main {
 
+     static  class Product {
+         public int product(int x , int y) {
+             return x * y;
+         }
+
+         public int product(int x, int y, int z) {
+             return x * y * z;
+         }
+
+         public double product(double x, double y) {
+             return x * y;
+         }
+
+         public static void main(String[] args) {
+             Product p = new Product();
+
+             int result1 = p.product(4, 5);
+             System.out.println("Product of two integers:" + result1);
+
+             int result2 = p.product(2, 3, 4);
+             System.out.println("Product of three integers:" + result2);
+
+             double result3 = p.product(4.5, 5.5);
+             System.out.println("Product of two doubles:" + result3);
+         }
+     }
+
+
 }
